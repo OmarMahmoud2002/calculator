66bc86b42a1f36d40e894c06e1bda3712c6fd0ba
OmarMahmoud2002 <omarmahmoud.devv@gmail.com>
2025-08-16 16:07:16 +0300

    feat: add scientific buttons and restructure calculator layout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
-import ButtonBox from './components/ButtonBox'
 import Button from './components/Button'
 import History from "./components/History";
 import CalcProvider from "./context/CalcContext";
 
 const btnValues = [
-  ["C", "+/-", "%", "/"],
-  [7, 8, 9, "x"],
-  [4, 5, 6, "-"],
-  [1, 2, 3, "+"],
+  ["C", "+/-", "%"],
+  [7, 8, 9],
+  [4, 5, 6],
+  [1, 2, 3],
   [0, ".", "="],
 ];
 
 function App() {
+  const scientificValues = ["√", "x²", "sin", "cos", "tan"];
+  const operatorValues = ["/", "x", "-", "+"];
+  
   return (
     <CalcProvider>
       <Wrapper>
         <Screen />
-        <ButtonBox>
-          {btnValues.flat().map((btn, i) => (
-            <Button
-              value={btn}
-              key={i}
-            />
-          ))}
-        </ButtonBox>
+        <div className="calculator-body">
+          <div className="scientific-row">
+            {scientificValues.map((btn, i) => (
+              <Button value={btn} key={`sci-${i}`} />
+            ))}
+          </div>
+          <div className="main-calculator">
+            <div className="left-panel">
+              {btnValues.flat().map((btn, i) => (
+                <Button value={btn} key={`main-${i}`} />
+              ))}
+            </div>
+            <div className="right-panel">
+              {operatorValues.map((btn, i) => (
+                <Button value={btn} key={`op-${i}`} />
+              ))}
+            </div>
+          </div>
+        </div>
         <History />
       </Wrapper>
     </CalcProvider>
